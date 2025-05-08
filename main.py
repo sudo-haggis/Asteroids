c98809f9b4373926967e0726f9e76262bdb3bd45
weedavedev <admin@wallacedevelopment.co.uk>
2025-05-08 13:41:00 +0200

    L5 complete commit, should do smaller file based commits, but fuck it, this is long and i need todo workgit add .git add .

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import * 
 
 def main():
     print(f"Starting Asteroids!")
@@ -13,15 +14,24 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill("#000000")
+
+        #re-draw player
+        player.draw(screen)
+
+        #redraw  map, requested todo last (after player? weird?)        
         pygame.display.flip()
 
+        #calc time taken to re-draw screen
         dt =  clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
