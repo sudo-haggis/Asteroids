f2380109ce88ae33ecfa33b3e73e3948f92eed7d
weedavedev <admin@wallacedevelopment.co.uk>
2025-05-08 12:50:34 +0200

    updated dt line to single line, what i had originally i may add, but changed to debug what boots was on about, returning to simple readable one liner like the solution

--- main.py
+++ main.py
@@ -21,8 +21,7 @@ def main():
         screen.fill("#000000")
         pygame.display.flip()
 
-        passed_time = clock.tick(60)
-        dt = passed_time / 1000
+        dt =  clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
