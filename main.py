62318d23b4cdecd3f9cc2906327051d2624a8976
weedavedev <admin@wallacedevelopment.co.uk>
2025-05-08 12:48:21 +0200

    Added clocking functionalitry to keep a grasp on the FPS

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
@@ -17,6 +20,9 @@ def main():
 
         screen.fill("#000000")
         pygame.display.flip()
+
+        passed_time = clock.tick(60)
+        dt = passed_time / 1000
 
 if __name__ == "__main__":
     main()
