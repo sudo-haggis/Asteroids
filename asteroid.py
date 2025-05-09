eba8263471ea10964e2a78fc09147d28a3a9932b
weedavedev <admin@wallacedevelopment.co.uk>
2025-05-09 12:00:19 +0200

    solution based update in asteroid creation and init, silly things really, need to remember about OOP, inhertiing self goes straight down into child instance, doesnt need re declaring

--- asteroid.py
+++ asteroid.py
@@ -3,8 +3,6 @@ from circleshape import *
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
-        self.radius = radius
-        self.velocity = 0
 
     def draw(self, screen):
         pygame.draw.circle(screen, "white", self.position, self.radius, 2)
@@ -14,4 +12,4 @@ class Asteroid(CircleShape):
         self.move(dt)
 
     def move(self, dt) :
-        self.position += self.velocity
+        self.position += self.velocity * dt
